3f394dd706a00cd86267aa0cfa565bd348f391c3
misogil0116 <lack_un@yahoo.co.jp>
2017-07-12 14:13:08 +0900

    Encoder Decoderの簡易なもの

--- EncDecAD.py
+++ EncDecAD.py
@@ -1,6 +1,58 @@
 import chainer
+import chainer.functions as F
+import chainer.links as L
+from preprocessing import PreprocessClass
 
 
-class EncDecAdClass(chainer.chain):
-    def __init__(self):
-        pass
+class EncDecAdClass(chainer.Chain):
+    def __init__(self, m, c, epoch):
+        super().__init__(
+            H=L.LSTM(m, c),
+            W=L.Linear(c, m)
+        )
+        self.preprocess = PreprocessClass()
+        self.preprocess.load_csv()
+        self.preprocess.divide_train_and_test()
+        self.train = self.preprocess.train_csv0
+        self.epoch = epoch
+
+    def forward(self):
+        self.H.reset_state()
+        h = None
+        accum_loss = None
+        for data50dim in self.train:
+            for data in data50dim:
+                h = self.H(data)
+            next_data = self.W(h)
+            accum_loss = F.mean_squared_error(next_data, data50dim[-1]) # 最初の二乗誤差
+            for i in reversed(range(49)): # 一番最後のものは上で計算済み
+                h = self.H(next_data)
+                next_data = self.W(h)
+                accum_loss += F.mean_squared_error(next_data, data50dim[i])
+
+        return accum_loss
+
+
+if __name__ == '__main__':
+    # setup model
+    epoch = 100
+    model = EncDecAdClass(1, 10, epoch)
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+
+    for i in range(model.epoch):
+        loss = model.forward()
+        loss.backward()
+        optimizer.update()
+        print("%d epoch" % i, str(loss))
+
+
+
+
+
+
+
+
+
+
+
